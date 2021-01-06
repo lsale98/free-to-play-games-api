65459e18e83b359f28a48da20b2c05a83f1e3086
Aleksandar <lakatos.aleksandar79@gmail.com>
2021-01-06 01:33:58 +0100

    Game genres added in navigation with animation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import { Category } from '../models/Category';
+import { ICategory } from '../models/Category';
+
 
 @Component({
   selector: 'app-navigation',
@@ -9,30 +10,55 @@ import { Category } from '../models/Category';
 })
 export class NavigationComponent implements OnInit {
 
-  duplicateCategories:Array<Category>=[]; 
-
-  categories: Array<Category>=[]; 
-  
   constructor(private apiService: ApiService) { }
 
+  genreMenuOpen: boolean = false;
+  menuOpen: boolean = false;
+
+  categories: ICategory[] = [
+    {
+      name: "MMO"
+    },
+     {
+      name: "MMORPG"
+    },
+    {
+      name: "Shooter"
+    },
+    {
+      name: "Strategy"
+    },
+    {
+      name: "Moba"
+    },
+    {
+      name: "Card Games"
+    },
+    {
+      name: "Racing"
+    },
+    {
+      name: "Sports"
+    },
+    {
+      name: "Social"
+    },
+    {
+      name: "Fighting"
+    },
+  ];
+
   ngOnInit(): void {
-    this.getCategories();
-    //console.log(this.categories);
-    console.log(this.duplicateCategories);
-    
   }
 
-  getCategories(): void {
-    this.apiService.getGames().subscribe(categories => {
-      for (const category of (categories as any)) {
-        this.duplicateCategories.push({
-          name: category.genre
-        });
-      }
-    });  
-    
-   
-    
+  toggleGenreMenu(): void{
+    this.genreMenuOpen = !this.genreMenuOpen;
   }
 
+  toggleMenu(): void{
+    this.menuOpen = !this.menuOpen;
+    this.genreMenuOpen = false;
+  }
+
+  
 }

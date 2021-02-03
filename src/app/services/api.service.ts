bb9e332dad990e646f09406879a562c4fed3815d
Aleksandar <lakatos.aleksandar79@gmail.com>
2021-02-03 19:01:24 +0100

    Deploy To Heroku

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IGame } from '../models/Game';
 @Injectable({
@@ -7,18 +7,21 @@ import { IGame } from '../models/Game';
 })
 export class ApiService {
 
-  readonly API_BASE = "https://www.freetogame.com/api/games";
+  readonly API_BASE = "https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api/games";
 
-  readonly API_BASE_SINGLE = "https://www.freetogame.com/api/game?id=";
+  readonly API_BASE_SINGLE = "https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api/game?id=";
 
-  readonly API_BASE_GENRE = "https://www.freetogame.com/api/games?category=";
+  readonly API_BASE_GENRE = "https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api/games?category=";
 
-  readonly API_BASE_PLATFORM = "https://www.freetogame.com/api/games?platform=";
+  readonly API_BASE_PLATFORM = "https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api/games?platform=";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+     
+   
+  }
 
   getGames(): Observable<HttpResponse<IGame[]>> {
-    return this.http.get<IGame[]>(this.API_BASE, { observe: 'response'});
+    return this.http.get<IGame[]>(this.API_BASE, { observe: 'response' });
   }
 
   getGame(id: Number): Observable<HttpResponse<IGame>>{

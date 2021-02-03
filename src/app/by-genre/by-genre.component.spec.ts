cd08ec7c43083b21c7b86a7de7a8b5e18fda2b81
Aleksandar <lakatos.aleksandar79@gmail.com>
2021-02-03 17:24:39 +0100

    Deploy to heroku

--- src/app/by-genre/by-genre.component.spec.ts
+++ src/app/by-genre/by-genre.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ByGenreComponent } from './by-genre.component';
 
@@ -6,7 +6,7 @@ describe('ByGenreComponent', () => {
   let component: ByGenreComponent;
   let fixture: ComponentFixture<ByGenreComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ByGenreComponent ]
     })

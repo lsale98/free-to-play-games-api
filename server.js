40365b77e1b6787849e87494b39ae4934b4715dc
Aleksandar <lakatos.aleksandar79@gmail.com>
2021-02-03 19:08:03 +0100

    Deploy To Heroku

--- server.js
+++ server.js
@@ -3,13 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
-    next();
-});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/free-to-play-games'));
 
